c3b53643152e007f399de8f6fd75002a2af1fdad
SeanDragon <syl8023who@gmail.com>
2017-04-18 17:59:28 +0800

    1 替换了过时方法

--- src/test/java/sd/Test_20170412.java
+++ src/test/java/sd/Test_20170412.java
@@ -1,10 +1,16 @@
 package sd;
 
+import pro.tools.system.ToolClassSearch;
+
+import java.util.Set;
+
 /**
  * Created by tuhao on 2017/4/12.
  */
 public class Test_20170412 {
 
     public static void main(String[] args) {
+        Set<Class<?>> allClazz = ToolClassSearch.getAllClazz();
+        allClazz.forEach(System.out::println);
     }
 }

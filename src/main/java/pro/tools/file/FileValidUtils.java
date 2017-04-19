027073bf93eb7e3c1d663e424737515018ab4f5d
tuhao <18063085192@163.com>
2017-04-19 10:24:39 +0800

    将部分没有注释的方法添加了注释

--- src/main/java/pro/tools/file/FileValidUtils.java
+++ src/main/java/pro/tools/file/FileValidUtils.java
@@ -14,18 +14,33 @@ public final class FileValidUtils {
         throw new UnsupportedOperationException("u can't instantiate me...");
     }
 
+    /**
+     * 是否是一个文件路径
+     * @param filePath 文件路径
+     * @return
+     */
     public static boolean isFile(String filePath) {
         if (!isFileExists(filePath)) return false;
         Path path = Paths.get(filePath);
         return Files.isRegularFile(path);
     }
 
+    /**
+     * 是否是一个目录
+     * @param filePath 文件夹路径
+     * @return
+     */
     public static boolean isDir(String filePath) {
         if (!isFileExists(filePath)) return false;
         Path path = Paths.get(filePath);
         return Files.isDirectory(path);
     }
 
+    /**
+     * 不是一个文件夹且一定是一个文件
+     * @param filePath 文件路径
+     * @return
+     */
     public static boolean readable(String filePath) {
         if (!isFileExists(filePath)) return false;
         Path path = Paths.get(filePath);
@@ -35,6 +50,11 @@ public final class FileValidUtils {
     }
 
 
+    /**
+     * 文件或目录是否存在
+     * @param filePath 文件或目录路径
+     * @return
+     */
     public static boolean isFileExists(String filePath) {
         return Files.exists(Paths.get(filePath));
     }
